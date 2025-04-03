34572f264fa482d6f931be92a29d112f16b9bff5
Luís Felipe Marciano <lfamarciano@gmail.com>
2025-04-03 13:04:58 -0300

    full lab5

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -3,14 +3,19 @@
     export let hLevel = 2;
 </script>
 
-<article>
-    <!-- <h2>{data.title}</h2> -->
-    <svelte:element this={"h" + hLevel}>{data.title}</svelte:element>
-    <img src={data.image} alt="" />
-    <p>
-        {data.description}
-    </p>
-</article>
+
+<div class="projects">
+    <article>
+        <svelte:element this={"h" + hLevel}>{data.title}</svelte:element>
+        <img src={data.image} alt="" />
+        <p>
+            {data.description}
+            <br/>
+            {data.year}
+        </p>
+    </article>
+    
+</div>
 
 <style>
     p {
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,16 +1,56 @@
 <svelte:head>
   <title>My projects</title>
 </svelte:head>
+
 <script>
+    import Pie from '$lib/Pie.svelte';
     import projects from "$lib/projects.json";
     import Project from "$lib/Project.svelte";
+    import * as d3 from 'd3';
+    // let rolledData = d3.rollups(projects, v => v.length, d => d.year);
+    let pieData;
+
+    $: {
+        // Initialize to an empty object every time this runs
+        pieData = {};
+        
+        // Calculate rolledData and pieData based on filteredProjects here
+        let rolledData = d3.rollups(filteredProjects, v => v.length, d => d.year);
+
+        // We don't need 'let' anymore since we already defined pieData
+        pieData = rolledData.map(([year, count]) => {
+            return { value: count, label: year };
+        });
+    }
+
+    let selectedYearIndex = -1;
+    let selectedYear;
+    $: selectedYear = selectedYearIndex > -1 ? pieData[selectedYearIndex].label : null;
+
+    let query = "";
+    $: filteredProjects = projects.filter(project => {
+        let values = Object.values(project).join("\n").toLowerCase();
+        return values.includes(query.toLowerCase());
+    });
+    $: filteredByYear = filteredProjects.filter(project => {
+        if (selectedYear) {
+            return project.year === selectedYear;
+        }
+
+        return true;
+    });
 </script>
 
 <!-- <pre>{ JSON.stringify(projects, null, "\t") }</pre>   -->
 
 <h1>{ projects.length} Projects</h1>
+
+<Pie data={pieData} bind:selectedIndex={selectedYearIndex} />
+
+<input type="search" bind:value={query} aria-label="Search projects" placeholder="🔍 Search projects..." />
+
 <div class="projects">
-    {#each projects as p}
+    {#each filteredByYear as p}
         <Project data={p}/>
       <!-- <article>
         <h2>{p.title}</h2>
